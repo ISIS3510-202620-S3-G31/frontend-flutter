eeaf58b138f53261d47a4849a524d318062bf227
YefranC <y.cespedes@uniandes.edu.co>
2026-09-19 02:25:59 -0500

    "Implementation custom breathing"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'screens/emotional_detective/emotional_detective_screen.dart';
 import 'screens/photo_of_the_day/photo_of_the_day_screen.dart';
 import 'screens/scream_tank/scream_tank_screen.dart';
 import 'screens/tear_collection/tear_collection_screen.dart';
 import 'theme/app_colors.dart';
+import 'screens/custom_breathing/custom_breathing_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -55,6 +57,18 @@ class HomeScreen extends StatelessWidget {
               FilledButton(
                 onPressed: () => _open(context, const ScreamTankScreen()),
                 child: const Text('Scream Tank'),
+              ),
+              FilledButton(
+                onPressed: () => _open(context, const TearCollectionScreen()),
+                child: const Text('Tear Collection'),
+              ),
+              FilledButton(
+                onPressed: () => _open(context, const EmotionalDetectiveScreen()),
+                child: const Text('Sprout Detective'),
+              ),
+              FilledButton(
+                onPressed: () => _open(context, const CustomBreathingScreen()),
+                child: const Text('Custom Breathing'),
               ),
             ],
           ),
